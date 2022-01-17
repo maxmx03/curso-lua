642e6457808271b3fb1c4ba10ce0b5d865ae97ba
Max Miliano <milianordelcanto@gmail.com>
2022-01-17 11:04:20 -0300

    fourth lesson external-world

---
+++ external-world.lua
@@ -0,0 +1,10 @@
+require("utils")
+
+io.write("Insira seu nome: ")
+local nome = io.read() -- vai ler strings
+
+io.write("Insira sua idade: ")
+local idade = io.read("n") -- vai ler números
+
+write("Seu nome: ", nome)
+write("Sua idade: ", idade)

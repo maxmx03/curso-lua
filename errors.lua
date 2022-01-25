be7cb474cab24bd1884bfce902e01f5a5f191cff
Max Miliano <milianordelcanto@gmail.com>
2022-01-25 10:21:47 -0300

    error added

---
+++ errors.lua
@@ -0,0 +1,47 @@
+require"utils"
+
+section "error function"
+write "Digite um valor"
+local numero = io.read("n")
+
+if type(numero) ~= "number" then
+  error "Você não digitou um número"
+end
+
+section "assert function"
+write "Digite um valor"
+local numero = assert(io.read("n"), "Você não digitou um número")
+write(numero)
+
+section "pcall"
+io.write("Digite o dividendo: ")
+local dividendo = assert(io.read("n"), "Valor inválido na linha 21")
+
+io.write("Digite o divisor: ")
+local divisor = assert(io.read("n"), "Valor inválido na linha 22")
+
+local status, err = pcall(function ()
+  local messages = {message = "Não pode dividir um número por 0"}
+
+  if divisor == 0 then
+    error(messages)
+  else
+    local calculo = dividendo / divisor
+    write(calculo)
+  end
+end)
+
+if not status then
+  write(err.message)
+end
+
+section "error level"
+local function hello(text) 
+  if type(text) ~= "string" then
+    error("A função hello precisa de um string como parametro", 2)
+  else
+    io.write(text)
+  end
+end
+
+hello({name = "Max"})

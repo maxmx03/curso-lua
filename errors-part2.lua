7b8feeab0850a94c1ff14c082c889b5f9c15a6f3
Max Miliano <milianordelcanto@gmail.com>
2022-02-04 12:05:26 -0300

    update errors

--- errors-part2.lua
+++ errors-part2.lua
@@ -1,26 +1,29 @@
 require "utils"
 
-section "pcall"
+section "errors - error, assert, pcall"
+
 local function divisao(dividendo, divisor)
-  if divisor == 0 then
+  if (divisor == 0) then
     error "Divisor não pode ser igual a zero"
   end
 
-  return dividendo / divisor
+  return dividendo // divisor
 end
 
 io.write("Digite o dividendo: ")
-local dividendo = assert(io.read("n"), "Valor inválido na linha 21")
+local dividendo = assert(io.read("n"), "Por favor digite um número na linha 13")
 
 io.write("Digite o divisor: ")
-local divisor = assert(io.read("n"), "Valor inválido na linha 22")
+local divisor = assert(io.read("n"), "Por favor digite um número na linha 14")
 
-local status, resultado = pcall(divisao, dividendo, divisor)
+local success, resultado = pcall(divisao, dividendo, divisor)
 
-if not status then
+if (not success) then
   local err = resultado
 
-  write(string.format("Mensagem de error: %s", err))
+  write(string.format("Error: %s", err))
 else
-  write(string.format("Resultado da divisão: %s", resultado))
+  write(string.format("Success: %s", resultado))
 end
+
+write("O programa chegou até aqui por causa do pcall")
